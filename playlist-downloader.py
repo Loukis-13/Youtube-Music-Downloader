a9c905e3b93faa09cf9d2f3eb94a5e170123238a
Loukis-13 <jose.hypolito@protonmail.com>
2023-03-15 12:16:44 -0300

    fixed download of age restricted videos

--- playlist-downloader.py
+++ playlist-downloader.py
@@ -3,7 +3,8 @@
 import sys
 import os
 import requests
-from pytube import Playlist
+from subprocess import run
+from pytube import Playlist, exceptions
 
 
 playlist = Playlist(sys.argv[1])
@@ -18,28 +19,31 @@ os.chdir(album)
 thumbnail = f"{album}.jpg"
 with open(thumbnail, "wb") as f: 
     f.write(requests.get(videos[0].thumbnail_url).content)
-os.system(f"convert '{thumbnail}' -crop 360x360+140+60 '{thumbnail}'")
+run(["convert", thumbnail, "-crop", "360x360+140+60", thumbnail])
 
 album_length = len(videos)
 for i, video in enumerate(playlist.videos, 1):
-    for _ in range(5):
-        try:
-            if not os.path.exists(f"{video.title}.mp3"):
+    if not os.path.exists(f"{video.title}.mp3"):
+        for _ in range(3):
+            try:
+                if video.age_restricted and not video.use_oauth:
+                    video.use_oauth = True
+
                 title = video.title
                 print(title)
-                video.streams.get_audio_only().download(filename=title+".mp4")
-                os.system(
-                    f"ffmpeg -loglevel 16 \
-                    -i '{title}.mp4' \
-                    -i '{thumbnail}' \
-                    -map 0 -map 1 \
-                    -metadata title='{title}' \
-                    -metadata artist='{artist}' \
-                    -metadata album='{album}' \
-                    -metadata track='{i}/{album_length}' \
-                    '{title}.mp3'")
-                os.system(f"rm '{title}.mp4'")
-            break
-        except:
-            pass
-
+                video.streams.get_audio_only().download(filename=f"{title}.mp4")
+                run([
+                    "ffmpeg", "-loglevel", "16",
+                    "-i", f"{title}.mp4",
+                    "-i", thumbnail,
+                    "-map", "0", "-map", "1",
+                    "-metadata", f"title='{title}'",
+                    "-metadata", f"artist='{artist}'",
+                    "-metadata", f"album='{album}'",
+                    "-metadata", f"track='{i}/{album_length}'",
+                    f"{title}.mp3"
+                ])
+                run(["rm", f"{title}.mp4"])
+                break
+            except exceptions.PytubeError as err:
+                print(str(err))
