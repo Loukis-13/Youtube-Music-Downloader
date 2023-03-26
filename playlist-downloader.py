7f8f4bc87245fb8fd4ffd705ee8dd485875b5091
Loukis-13 <jose.hypolito@protonmail.com>
2023-03-26 18:17:53 -0300

    removed alive_progress

--- playlist-downloader.py
+++ playlist-downloader.py
@@ -4,9 +4,7 @@ import sys
 import os
 import requests
 from subprocess import run
-from pytube import Playlist, exceptions
-from time import sleep
-from alive_progress import alive_bar
+from pytube import Playlist
 
 
 playlist = Playlist(sys.argv[1])
@@ -26,35 +24,26 @@ if not os.path.exists(cover):
     run(["convert", cover, "-crop", "360x360+140+60", cover])
 
 for i, video in enumerate(playlist.videos, 1):
-    for _ in range(3):
-        try:
-            file = video.streams.get_audio_only()
-            title = file.title
-            url = file.url
-            default_filename = f"{file.default_filename.removesuffix('.mp4')}.mp3"
+    file = video.streams.get_audio_only()
+    title = file.title
+    url = file.url
+    default_filename = f"{file.default_filename.removesuffix('.mp4')}.mp3"
 
-            if not os.path.exists(default_filename):
-                with alive_bar(2, title=f"[{i}/{album_length}] {title}", stats=False, title_length=30, dual_line=True) as bar:
-                    bar.text = "Checking age restriction"
-                    if video.age_restricted and not video.use_oauth:
-                        video.use_oauth = True
-                    bar()
+    if not os.path.exists(default_filename):
+        print(f"[{i}/{album_length}] {title}")
 
-                    bar.text = "Downloading video, converting to .mp3 and adding metadata"
-                    run([
-                        "ffmpeg",
-                        "-loglevel", "16",
-                        "-i", url,
-                        "-i", cover,
-                        "-map", "0", "-map", "1",
-                        "-metadata", f"title={title}",
-                        "-metadata", f"artist={artist}",
-                        "-metadata", f"album={album}",
-                        "-metadata", f"track={i}/{album_length}",
-                        default_filename
-                    ])
-                    bar()
-            break
-        except exceptions.PytubeError as err:
-            print(str(err))
-            sleep(2)
+        if video.age_restricted and not video.use_oauth:
+            video.use_oauth = True
+
+        run([
+            "ffmpeg",
+            "-loglevel", "16",
+            "-i", url,
+            "-i", cover,
+            "-map", "0", "-map", "1",
+            "-metadata", f"title={title}",
+            "-metadata", f"artist={artist}",
+            "-metadata", f"album={album}",
+            "-metadata", f"track={i}/{album_length}",
+            default_filename
+        ])
